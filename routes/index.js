407ccf2a0371edb1c15b45b9c29fcd949ac295fd
Alessio Zappa <alessioedoardozappa@gmail.com>
2016-06-23 16:46:14 +0200

    base64 png url

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,45 @@ module.exports = {
       throw new Error(e);
     }
 
-    const userPalette = palette[Math.floor((Math.random() * palette.length))];
-    res.send(`<pre>${JSON.stringify(userPalette.colors, null, 2)}</pre>`);
+    let userPalette = palette[Math.floor((Math.random() * palette.length))].colors;
+    userPalette = userPalette.map((c) => `#${c}`);
+
+    ctx.rect(0, 0, canvasOpts.canvasM, canvasOpts.canvasM);
+    ctx.fillStyle = userPalette[Math.floor((Math.random() * userPalette.length))];
+    ctx.fill();
+
+    for (let c = 0; c <= canvasOpts.perRow; c++) {
+      for (let r = 0; r <= canvasOpts.perRow; r++) {
+        let reverse = r;
+
+        if (c % 2 === 0) {
+          reverse = reverse - 0.5;
+        }
+
+        if (Math.random() > (1 - canvasOpts.fillPerc)) {
+          ctx.beginPath();
+          ctx.moveTo(((reverse - 0.5) * mulW), (c * mulH));
+          ctx.lineTo(((reverse + 0.5) * mulW), (c * mulH));
+          ctx.lineTo((reverse * mulW), ((c + 1) * mulH));
+
+
+          ctx.fillStyle = userPalette[Math.floor((Math.random() * userPalette.length))];
+          ctx.fill();
+          ctx.closePath();
+        }
+
+        if (Math.random() > (1 - canvasOpts.fillPerc)) {
+          ctx.beginPath();
+          ctx.moveTo(((reverse + 1) * mulW), ((c + 1) * mulH));
+          ctx.lineTo(((reverse + 0.5) * mulW), (c * mulH));
+          ctx.lineTo((reverse * mulW), ((c + 1) * mulH));
+
+          ctx.fillStyle = userPalette[Math.floor((Math.random() * userPalette.length))];
+          ctx.fill();
+          ctx.closePath();
+        }
+      }
+    }
+    res.send(`<img src="${canvas.toDataURL()}" />`);
   },
 };
